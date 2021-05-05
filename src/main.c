91dcf5d77d874110ef73462799f62d8bdd822283
Siddharth <sidh.baichoo@gmail.com>
2021-05-05 18:50:19 -0400

    essaie de la 6 v15

--- src/main.c
+++ src/main.c
@@ -288,7 +288,8 @@ error_code find_file_descriptor(FILE *archive, BPB *block, char *path, FAT_entry
     }
     int numBlocks = 0;
     //on commence a partir du premier secteur de données
-    u_int32_t begin = as_uint16(block->BPB_RsvdSecCnt) + as_uint32(block->BPB_HiddSec) + ((block->BPB_NumFATs) * as_uint32(block->BPB_FATSz32));
+    u_int32_t FAT_start = as_uint16(block->BPB_RsvdSecCnt) + as_uint32(block->BPB_HiddSec) + (block->BPB_NumFATs * as_uint32(block->BPB_FATSz32));
+    u_int32_t begin = cluster_to_lba(block, 0, FAT_start);
     printf("\nbegin = %d\n", begin);
     uint32_t currentCluster = 2;
     uint32_t current_logical_address;
